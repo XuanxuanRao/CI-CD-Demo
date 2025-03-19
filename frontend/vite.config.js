1eb5b4c7bdf449fc7e5083c20263d3ccd443f470
b1t519 <buaa_zjq@163.com>
2025-03-19 19:54:24 +0800

    张珺强触发的CI/CD

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
